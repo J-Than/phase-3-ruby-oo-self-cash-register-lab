cb0bf7fa5d1f1cd15d6ea8afc54c81bdec7ad23c
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-11 20:59:12 -0400

    Passes 11 tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CashRegister
 
-  attr_accessor  :total, :prior_total, :pre_discount_total
+  attr_accessor  :total, :prior_total, :pre_discount_total, :items
   attr_reader :discount
   attr_writer :z
 
@@ -11,17 +11,23 @@ class CashRegister
     @pre_discount_total = 0
     @prior_total = 0
     @total = 0
+    @items = []
   end
 
   def add_item(item_name, price, quantity = 1)
     self.prior_total = @pre_discount_total
     self.pre_discount_total += price * quantity
     self.total = @pre_discount_total
+    self.items.push(item_name)
   end
 
   def apply_discount
-    self.total = @pre_discount_total * ( 1.0 - @discount / 100.0 )
-    "After the discount, the total comes to $#{self.total.to_i}."
+    if @discount > 0 && @discount <= 100 then
+      self.total = @pre_discount_total * ( 1.0 - @discount / 100.0 )
+      return "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
   end
 
 end

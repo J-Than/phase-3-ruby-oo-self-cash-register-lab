c6f045c94a821f009a735ddbb37d8f9a6a72b5bf
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-11 20:52:48 -0400

    Passes all tests except final

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,31 @@
+require 'pry'
+
 class CashRegister
 
-  attr_accessor  :total, :prior_total
+  attr_accessor  :total, :prior_total, :pre_discount_total
   attr_reader :discount
   attr_writer :z
 
   def initialize(discount = 0)
     @discount = discount
+    @pre_discount_total = 0
     @prior_total = 0
     @total = 0
   end
 
   def add_item(item_name, price, quantity = 1)
-    self.prior_total = @total
-    self.total += price * quantity
+    self.prior_total = @pre_discount_total
+    self.pre_discount_total += price * quantity
+    self.total = @pre_discount_total
+  end
+
+  def apply_discount
+    self.total = @pre_discount_total * ( 1.0 - @discount / 100.0 )
+    "After the discount, the total comes to $#{self.total.to_i}."
   end
 
 end
+
+# cash_register_with_discount = CashRegister.new(20)
+# cash_register_with_discount.add_item("expensive", 1000)
+# cash_register_with_discount.apply_discount

1940e6deb0f14a38198eabded5e4447de493112b
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-11 20:27:09 -0400

    Passes first two tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class CashRegister
 
+  attr_accessor :x
+  attr_reader :discount, :total
+  attr_writer :z
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
+
+end

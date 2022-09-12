3d957411b2882e04ded2e42e45ca7044ad394eaf
Jarod Wilson <jaroddwilson@gmail.com>
2022-09-11 20:32:30 -0400

    Passes first six tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,18 @@
 class CashRegister
 
-  attr_accessor :x
-  attr_reader :discount, :total
+  attr_accessor  :total, :prior_total
+  attr_reader :discount
   attr_writer :z
 
   def initialize(discount = 0)
     @discount = discount
+    @prior_total = 0
     @total = 0
+  end
+
+  def add_item(item_name, price, quantity = 1)
+    self.prior_total = @total
+    self.total += price * quantity
   end
 
 end
